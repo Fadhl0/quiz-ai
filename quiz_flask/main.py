6fa907616e9dffc7a4a52280f5357d3babfc137f
Fadhl Al Fadhili <fadhlhayderr@gmail.com>
2026-03-08 19:09:12 +0300

    fix hf deprcated parameter

--- quiz_flask/main.py
+++ quiz_flask/main.py
@@ -53,8 +53,7 @@ def download_from_hf(id, file):
         file_path = hf_hub_download(
             repo_id=id, 
             filename=file,
-            local_dir="../LLM",
-            local_dir_use_symlinks=False
+            local_dir="../LLM"
         )
     except Exception as e:
         print(f"Hugging Face download failed: {e}")
